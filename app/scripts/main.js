17e2d90e261720869a62d2f29f44c830e2601656
Josh Daniels <daniels.joshua100@gmail.com>
2014-06-16 23:43:41 -0400

    finished. pushing to master, then gh-pages

---
+++ app/scripts/main.js
@@ -0,0 +1,32 @@
+$(".accordian-tab-1").on("click", function() {
+
+
+	if($(".accordian-bellows-1").is(":visible")) {
+		$(".accordian-bellows-1").hide()
+	}else{
+		$(".accordian-bellows-1").show()
+	}
+
+});
+
+$(".accordian-tab-2").on("click", function() {
+
+
+	if($(".accordian-bellows-2").is(":visible")) {
+		$(".accordian-bellows-2").hide()
+	}else{
+		$(".accordian-bellows-2").show()
+	}
+
+});
+
+$(".accordian-tab-3").on("click", function() {
+
+
+	if($(".accordian-bellows-3").is(":visible")) {
+		$(".accordian-bellows-3").hide()
+	}else{
+		$(".accordian-bellows-3").show()
+	}
+
+});
